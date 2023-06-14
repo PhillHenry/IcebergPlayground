9d4a197deba4f795d6a30c7b12806c70154dfeae
PhillipHenry <PhillHenry@gmail.com>
2023-06-14 10:46:22 +0100

    New assertion. Fixed missing dependencies.

--- src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
@@ -7,16 +7,27 @@ import uk.co.odinconsultants.SparkForTesting._
 class IcebergCRUDSpec extends AnyWordSpec {
   val IntField: String = "_1"
   "Spark" when {
-    "given our configuration" should {
+    "given a dataset to CRUD" should {
       val data: Seq[(Int, String)] = Seq((41, "phill"), (42, "henry"))
       val df: DataFrame            = spark.createDataFrame(data)
+      val tableName                = "spark_file_test_writeTo"
 
-      "read and write to metastore" in {
-        val tableName                = "spark_file_test_writeTo"
-        df.show()
+      "create the appropriate Iceberg files" in {
         df.writeTo(tableName).create()
-        val output: DataFrame        = spark.read.table(tableName)
+        val output: DataFrame = spark.read.table(tableName)
         assert(output.collect().length == data.length)
+      }
+
+      "support updates" in {
+        val newVal = "ipse locum"
+        spark.sqlContext.sql(s"update $tableName set _2='$newVal'")
+        val output: DataFrame = spark.read.table(tableName)
+        assert(output.collect().length == data.length)
+        for {
+          row <- output.collect()
+        } yield {
+          assert(row.getString(1) == newVal)
+        }
       }
     }
   }
