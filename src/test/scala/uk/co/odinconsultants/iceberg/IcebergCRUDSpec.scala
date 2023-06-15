3f5070bb003b98bceb3add6109cb6ddc2f78ee66
PhillipHenry <PhillHenry@gmail.com>
2023-06-15 14:12:28 +0100

    Refactor.

--- src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
@@ -1,40 +1,42 @@
 package uk.co.odinconsultants.iceberg
 
-import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.scalatest.GivenWhenThen
 import org.scalatest.wordspec.AnyWordSpec
 import uk.co.odinconsultants.SparkForTesting._
 import uk.co.odinconsultants.SpecFormats.prettyPrintSampleOf
+import org.scalatest._
+
 
 class IcebergCRUDSpec extends AnyWordSpec with GivenWhenThen {
-  val IntField: String = "_1"
+  import org.apache.spark.sql.Encoders._
   "A dataset to CRUD" should {
-    val data: Seq[(Int, String)] = Seq((41, "phill"), (42, "henry"))
-    val df: DataFrame            = spark.createDataFrame(data)
+    import spark.implicits._
     val tableName                = "spark_file_test_writeTo"
 
-    "create the appropriate Iceberg files" in {
+    "create the appropriate Iceberg files" in new SimpleFixture {
       Given(s"data\n${prettyPrintSampleOf(data)}")
       When(s"writing to table '$tableName'")
-      df.writeTo(tableName).create()
+      spark.createDataFrame(data).writeTo(tableName).create()
       Then("reading the table back yields the same data")
-      val output: DataFrame = spark.read.table(tableName)
-      assert(output.collect().length == data.length)
+      val output: Dataset[Datum] = spark.read.table(tableName).as[Datum]
+      assert(output.collect().toSet == data.toSet)
     }
 
     val newVal    = "ipse locum"
-    val updateSql = s"update $tableName set _2='$newVal'"
-    s"support updates with '$updateSql'" in {
+    val updateSql = s"update $tableName set label='$newVal'"
+    s"support updates with '$updateSql'" in new SimpleFixture {
       Given(s"SQL '$updateSql''")
       When("we execute it")
       spark.sqlContext.sql(updateSql)
       Then("all rows are updated")
-      val output: DataFrame = spark.read.table(tableName)
-      val rows: Array[Row]  = output.collect()
+      val output: Dataset[Datum] = spark.read.table(tableName).as[Datum]
+      val rows: Array[Datum]  = output.collect()
+      And(s"look like:\n${prettyPrintSampleOf(rows)}")
       assert(rows.length == data.length)
       for {
         row <- rows
-      } yield assert(row.getString(1) == newVal)
+      } yield assert(row.label == newVal)
     }
   }
 }
