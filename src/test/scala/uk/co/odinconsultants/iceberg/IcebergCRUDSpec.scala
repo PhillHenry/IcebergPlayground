6a64fda45e06376d0f9d13e5decde092a51e94d1
PhillipHenry <PhillHenry@gmail.com>
2023-06-15 09:46:34 +0100

    Given, when, then.

--- src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/IcebergCRUDSpec.scala
@@ -1,34 +1,39 @@
 package uk.co.odinconsultants.iceberg
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
+import org.scalatest.GivenWhenThen
 import org.scalatest.wordspec.AnyWordSpec
 import uk.co.odinconsultants.SparkForTesting._
 
-class IcebergCRUDSpec extends AnyWordSpec {
+class IcebergCRUDSpec extends AnyWordSpec with GivenWhenThen {
   val IntField: String = "_1"
-  "Spark" when {
-    "given a dataset to CRUD" should {
-      val data: Seq[(Int, String)] = Seq((41, "phill"), (42, "henry"))
-      val df: DataFrame            = spark.createDataFrame(data)
-      val tableName                = "spark_file_test_writeTo"
+  "A dataset to CRUD" should {
+    val data: Seq[(Int, String)] = Seq((41, "phill"), (42, "henry"))
+    val df: DataFrame            = spark.createDataFrame(data)
+    val tableName                = "spark_file_test_writeTo"
 
-      "create the appropriate Iceberg files" in {
-        df.writeTo(tableName).create()
-        val output: DataFrame = spark.read.table(tableName)
-        assert(output.collect().length == data.length)
-      }
+    "create the appropriate Iceberg files" in {
+      Given(s"data $data")
+      When(s"writing to table $tableName")
+      df.writeTo(tableName).create()
+      Then("reading the table back yields the same data")
+      val output: DataFrame = spark.read.table(tableName)
+      assert(output.collect().length == data.length)
+    }
 
-      "support updates" in {
-        val newVal = "ipse locum"
-        spark.sqlContext.sql(s"update $tableName set _2='$newVal'")
-        val output: DataFrame = spark.read.table(tableName)
-        assert(output.collect().length == data.length)
-        for {
-          row <- output.collect()
-        } yield {
-          assert(row.getString(1) == newVal)
-        }
-      }
+    val newVal    = "ipse locum"
+    val updateSql = s"update $tableName set _2='$newVal'"
+    s"support updates with '$updateSql'" in {
+      Given(s"SQL '$updateSql")
+      When("we execute it")
+      spark.sqlContext.sql(updateSql)
+      Then("all rows are updated")
+      val output: DataFrame = spark.read.table(tableName)
+      val rows: Array[Row]  = output.collect()
+      assert(rows.length == data.length)
+      for {
+        row <- rows
+      } yield assert(row.getString(1) == newVal)
     }
   }
 }
