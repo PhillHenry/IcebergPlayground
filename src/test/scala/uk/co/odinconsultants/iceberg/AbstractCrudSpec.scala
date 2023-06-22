9f0c558e2b7e9a9564854c3b59e1dac7c73a14ec
PhillipHenry <PhillHenry@gmail.com>
2023-06-22 09:50:14 +0100

    Refactor.

---
+++ src/test/scala/uk/co/odinconsultants/iceberg/AbstractCrudSpec.scala
@@ -0,0 +1,72 @@
+package uk.co.odinconsultants.iceberg
+import org.apache.iceberg.Table
+import org.apache.spark.sql.Dataset
+import org.scalatest.GivenWhenThen
+import org.scalatest.wordspec.AnyWordSpec
+import uk.co.odinconsultants.SparkForTesting._
+import uk.co.odinconsultants.iceberg.SQL.{createDatumTable, insertSQL}
+
+import scala.collection.mutable.{Set => MSet}
+
+abstract class AbstractCrudSpec extends AnyWordSpec with GivenWhenThen {
+
+  import spark.implicits._
+
+  def tableName: String
+  def mode: String
+
+  s"A $mode table" should {
+    val files: MSet[String] = MSet.empty[String]
+    val createSQL: String   = tableDDL(tableName, mode)
+    "create no new files for COW" in new SimpleFixture {
+      Given(s"SQL:\n'$createSQL")
+      When("we execute it")
+      spark.sqlContext.sql(createSQL)
+      val table: Table = icebergTable(tableName)
+      assert(table != null)
+      Then(s"the is an Iceberg table, $table")
+    }
+    "insert creates new files for COW" in new SimpleFixture {
+      val sql: String          = insertSQL(tableName, data)
+      Given(s"SQL:\n$sql")
+      When("we execute it")
+      spark.sqlContext.sql(sql)
+      files.addAll(dataFilesIn(tableName))
+      assert(files.nonEmpty)
+      Then(s"there are ${files.size} data files")
+      val output: Array[Datum] = andTheTableContains(tableName)
+      assert(output.toSet == data.toSet)
+    }
+    "update creates no new files for COW" in new SimpleFixture {
+      val toUpdate: Datum      = data.head
+      val fileCount: Int       = dataFilesIn(tableName).length
+      val sql: String          = s"UPDATE $tableName SET label='${toUpdate.label}X' WHERE id=${toUpdate.id}"
+      Given(s"SQL:\n$sql")
+      When("we execute it")
+      spark.sqlContext.sql(sql)
+      files.addAll(dataFilesIn(tableName))
+      assert(files.size == fileCount)
+      Then(s"there are now ${files.size} data files")
+      val output: Array[Datum] = andTheTableContains(tableName)
+      assert(output.toSet != data.toSet)
+    }
+  }
+
+  private def tableDDL(tableName: String, mode: String) = {
+    val createSQL: String = s"""${createDatumTable(tableName)} TBLPROPERTIES (
+                               |    'write.delete.mode'='$mode',
+                               |    'write.update.mode'='$mode',
+                               |    'write.merge.mode'='$mode'
+                               |) PARTITIONED BY (${classOf[
+                                Datum
+                              ].getDeclaredFields.head.getName}); """.stripMargin
+    createSQL
+  }
+
+  def andTheTableContains(tableName: String): Array[Datum] = {
+    val table: Dataset[Datum] = spark.read.table(tableName).as[Datum]
+    val rows: Array[Datum]    = table.collect()
+    And(s"the table contains:\n${rows.mkString("\n")}")
+    rows
+  }
+}
--- src/test/scala/uk/co/odinconsultants/iceberg/CowAndMorSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/CowAndMorSpec.scala
@@ -1,70 +1,8 @@
 package uk.co.odinconsultants.iceberg
-import org.apache.iceberg.Table
-import org.apache.spark.sql.Dataset
-import org.scalatest.GivenWhenThen
-import org.scalatest.wordspec.AnyWordSpec
-import uk.co.odinconsultants.SparkForTesting._
-import uk.co.odinconsultants.iceberg.SQL.{createDatumTable, insertSQL}
 
-import scala.collection.mutable.{Set => MSet}
+class CowAndMorSpec extends AbstractCrudSpec {
 
-class CowAndMorSpec extends AnyWordSpec with GivenWhenThen {
-  import spark.implicits._
+  override def tableName = "cow_table"
+  override def mode = "copy-on-write"
 
-  "A COW table" should {
-    val tableName           = "cow_table"
-    val files: MSet[String] = MSet.empty[String]
-    val mode                = "copy-on-write"
-    val createSQL: String   = tableDDL(tableName, mode)
-    "create no new files for COW" in new SimpleFixture {
-      Given(s"SQL:\n'$createSQL")
-      When("we execute it")
-      spark.sqlContext.sql(createSQL)
-      val table: Table = icebergTable(tableName)
-      assert(table != null)
-      Then(s"the is an Iceberg table, $table")
-    }
-    "insert creates new files for COW" in new SimpleFixture {
-      val sql: String          = insertSQL(tableName, data)
-      Given(s"SQL:\n$sql")
-      When("we execute it")
-      spark.sqlContext.sql(sql)
-      files.addAll(dataFilesIn(tableName))
-      assert(files.nonEmpty)
-      Then(s"there are ${files.size} data files")
-      val output: Array[Datum] = andTheTableContains(tableName)
-      assert(output.toSet == data.toSet)
-    }
-    "update creates no new files for COW" in new SimpleFixture {
-      val toUpdate: Datum      = data.head
-      val fileCount: Int       = dataFilesIn(tableName).length
-      val sql: String          = s"UPDATE $tableName SET label='${toUpdate.label}X' WHERE id=${toUpdate.id}"
-      Given(s"SQL:\n$sql")
-      When("we execute it")
-      spark.sqlContext.sql(sql)
-      files.addAll(dataFilesIn(tableName))
-      assert(files.size == fileCount)
-      Then(s"there are now ${files.size} data files")
-      val output: Array[Datum] = andTheTableContains(tableName)
-      assert(output.toSet != data.toSet)
-    }
-  }
-
-  private def tableDDL(tableName: String, mode: String) = {
-    val createSQL: String = s"""${createDatumTable(tableName)} TBLPROPERTIES (
-                       |    'write.delete.mode'='$mode',
-                       |    'write.update.mode'='$mode',
-                       |    'write.merge.mode'='$mode'
-                       |) PARTITIONED BY (${classOf[
-                                Datum
-                              ].getDeclaredFields.head.getName}); """.stripMargin
-    createSQL
-  }
-
-  def andTheTableContains(tableName: String): Array[Datum] = {
-    val table: Dataset[Datum] = spark.read.table(tableName).as[Datum]
-    val rows: Array[Datum]    = table.collect()
-    And(s"the table contains:\n${rows.mkString("\n")}")
-    rows
-  }
 }
