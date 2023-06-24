bf19bdfeb46fa2190fde506032fafdedb68e127f
PhillipHenry <PhillHenry@gmail.com>
2023-06-24 09:50:47 +0100

    Delete assertions.

--- src/test/scala/uk/co/odinconsultants/iceberg/MergeOnReadSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/MergeOnReadSpec.scala
@@ -1,6 +1,6 @@
 package uk.co.odinconsultants.iceberg
 
-import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.{DataFrame, Row}
 import uk.co.odinconsultants.SparkForTesting._
 
 class MergeOnReadSpec extends AbstractCrudSpec {
@@ -15,7 +15,7 @@ class MergeOnReadSpec extends AbstractCrudSpec {
       current: Set[String],
       changes: Set[Datum],
   ): Unit = {
-    val newFiles: Set[String]     = current -- previous
+    val newFiles: Set[String]       = current -- previous
     newFiles.foreach { file: String =>
       assert(
         file.endsWith("00001.parquet")
@@ -24,12 +24,20 @@ class MergeOnReadSpec extends AbstractCrudSpec {
           || file.endsWith("00001-deletes.parquet")
       )
     }
-    val parquetFiles: Set[String] = newFiles.filter(_.endsWith(".parquet"))
-    val deletes     : Set[String] = parquetFiles.filter(_.contains("-deletes."))
-    assert(deletes.nonEmpty)
-    deletes.foreach { file: String =>
-      val df: DataFrame = spark.read.parquet(file)
-      assert(df.count() == 1) // one row changed
+    val parquetFiles: Set[String]   = newFiles.filter(_.endsWith(".parquet"))
+    val deleteFiles: Set[String]    = parquetFiles.filter(_.contains("-deletes."))
+    assert(deleteFiles.nonEmpty)
+    deleteFiles.foreach { file: String =>
+      val df: DataFrame       = spark.read.parquet(file)
+      df.show(false)
+      val deletes: Array[Row] = df.collect()
+      assert(deletes.length == changes.size)
+      deletes.foreach { row: Row =>
+        val file_path: String              = row.getString(0)
+        val pos: Long                      = row.getLong(1)
+        val persistedChanges: Array[Datum] = spark.read.parquet(file_path).as[Datum].collect()
+        assert(changes.map(_.id).contains(persistedChanges(pos.toInt).id))
+      }
     }
     val nonDeleteFiles: Set[String] = parquetFiles.filter(!_.contains("-deletes."))
     assert(nonDeleteFiles.nonEmpty)
