faa72e6610e2291c178fc9e2cee0992abbbbfdc0
PhillipHenry <PhillHenry@gmail.com>
2023-11-04 10:14:53 +0000

    Caching does not mean we cannot see any changes.

--- src/test/scala/uk/co/odinconsultants/iceberg/CachingSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/CachingSpec.scala
@@ -24,13 +24,16 @@ class CachingSpec extends AnyWordSpec with GivenWhenThen {
       val output: Dataset[Datum] = spark.read.table(tableName).as[Datum]
       assert(output.collect().size == data.length)
 
-      When("we cache one Dataset and then append to the same table")
+      When("we cache one Dataset and then append more data to the same table")
       output.cache() // In Delta Lake, this would mean any further reads saw the data frozen in time at this point
       spark.createDataFrame(data).writeTo(tableName).append()
 
-      Then("reading the table back yields the same data")
+      Then("reading via a new Dataset reference shows the appended data")
       val post_cache: Dataset[Datum] = spark.read.table(tableName).as[Datum]
       assert(post_cache.collect().size == data.length * 2)
+
+      And("the old, cached reference still sees the old snapshot of data")
+      assert(output.collect().size == data.length)
     }
   }
 }
