d991c3b6dc355cde21eee884bc42ffb352865059
PhillipHenry <PhillHenry@gmail.com>
2023-11-16 13:44:43 +0000

    Stupid pythonic mistake

--- src/test/scala/uk/co/odinconsultants/iceberg/OptimizationSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/OptimizationSpec.scala
@@ -2,7 +2,7 @@ package uk.co.odinconsultants.iceberg
 import org.scalatest.GivenWhenThen
 import org.scalatest.wordspec.AnyWordSpec
 import uk.co.odinconsultants.SparkForTesting._
-import uk.co.odinconsultants.SpecFormats.{formatSQL, prettyPrintSampleOf}
+import uk.co.odinconsultants.SpecFormats.{formatSQL, prettyPrintSampleOf, toHumanReadable}
 
 class OptimizationSpec extends SpecPretifier with GivenWhenThen with TableNameFixture {
 
@@ -15,14 +15,15 @@ class OptimizationSpec extends SpecPretifier with GivenWhenThen with TableNameFi
       Given(s"data\n${prettyPrintSampleOf(data)}")
       When(s"writing $num_rows rows to table '$tableName'")
       spark.createDataFrame(data).writeTo(tableName).create()
-      val filesBefore = Set(dataFilesIn(tableName))
+      val filesBefore = dataFilesIn(tableName).toSet
       val sql =
         s"CALL system.rewrite_data_files(table => \"$tableName\", where => 'partitionKey = ${data.head.partitionKey}')"
+      And(s"The files are:\n${filesBefore.mkString("\n")}")
       And(s"we execute the SQL ${formatSQL(sql)}")
       spark.sqlContext.sql(sql)
-      val filesAfter = Set(dataFilesIn(tableName))
-      Then(s"the files added are:\n${(filesBefore -- filesAfter).mkString("\n")}")
-      And(s"the files removed are:\n${(filesAfter -- filesBefore).mkString("\n")}")
+      val filesAfter = dataFilesIn(tableName).toSet
+      Then(s"the files added to the original ${filesBefore.size} are:\n${(filesBefore -- filesAfter).mkString("\n")}")
+      And(s"the files removed from the subsequent ${filesAfter.size} are:\n${(filesAfter -- filesBefore).mkString("\n")}")
     }
   }
 
