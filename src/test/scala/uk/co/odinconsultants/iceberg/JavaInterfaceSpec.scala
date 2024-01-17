6b0972479b08c97569748d280abdc8c45923a759
PhillipHenry <PhillHenry@gmail.com>
2024-01-17 13:31:30 +0000

    Assertions

--- src/test/scala/uk/co/odinconsultants/iceberg/JavaInterfaceSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/JavaInterfaceSpec.scala
@@ -2,14 +2,16 @@ package uk.co.odinconsultants.iceberg
 import org.apache.iceberg.Table
 import org.apache.iceberg.hadoop.HadoopFileIO
 import org.scalatest.GivenWhenThen
-import uk.co.odinconsultants.documentation_utils.{SpecPretifier, TableNameFixture}
+import uk.co.odinconsultants.documentation_utils.{Datum, SpecPretifier, TableNameFixture}
 
 class JavaInterfaceSpec extends SpecPretifier with GivenWhenThen with TableNameFixture {
   "Iceberg tables" should {
     "have its history queried via the Java APIs" in new SimpleSparkFixture {
+      import spark.implicits._
       Given("a table that has seen changes")
       spark.createDataFrame(data).writeTo(tableName).create()
-      val updateSql = s"update $tableName set label='newVal'"
+      val newVal    = System.currentTimeMillis().toString
+      val updateSql = s"update $tableName set label='$newVal'"
       spark.sqlContext.sql(updateSql)
 
       When(s"we query the table")
@@ -17,9 +19,13 @@ class JavaInterfaceSpec extends SpecPretifier with GivenWhenThen with TableNameF
       val table: Table = icebergTable(tableName)
       val filenames    = MetaUtils.allFilesThatmake(table, new HadoopFileIO(hadoopConfig))
 
-      Then(s"the filenames are:\n${filenames.mkString("\n")}")
+      Then(
+        s"the filenames are:\n${filenames.mkString("\n")}\nand they contain the most recent data"
+      )
 
-      assert(spark.read.format("parquet").load(filenames.toArray: _*).count() == num_rows)
+      val rows = spark.read.format("parquet").load(filenames.toArray: _*).as[Datum].collect()
+      assert(rows.length == num_rows)
+      rows.foreach(x => assert(x.label == newVal))
     }
   }
 }
