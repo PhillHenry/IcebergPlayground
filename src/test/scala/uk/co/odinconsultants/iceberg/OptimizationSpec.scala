7c77b1ed16670946b3fb99411c815de9466a3708
PhillipHenry <PhillHenry@gmail.com>
2024-08-18 10:18:44 +0100

    Expire snapshots with a CALL

--- src/test/scala/uk/co/odinconsultants/iceberg/OptimizationSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/OptimizationSpec.scala
@@ -4,6 +4,8 @@ import org.scalatest.GivenWhenThen
 import uk.co.odinconsultants.SparkForTesting._
 import uk.co.odinconsultants.documentation_utils.{Datum, SpecPretifier, TableNameFixture}
 
+import java.text.SimpleDateFormat
+
 /**
  * Amogh Jahagirdar
 Expires snapshots does do a file cleanup as well, it's specifically cleaning up files which are no longer referenced after the snapshots are removed.
@@ -17,6 +19,8 @@ class OptimizationSpec extends SpecPretifier with GivenWhenThen with TableNameFi
 
   info("https://iceberg.apache.org/docs/1.4.2/spark-procedures/")
 
+  val MIN_INPUT_FILES: Int = 2
+
   s"A table that has many files" should {
     s"have those files aggregated" in new SimpleSparkFixture {
       override def num_rows: Int = 20000
@@ -27,7 +31,8 @@ class OptimizationSpec extends SpecPretifier with GivenWhenThen with TableNameFi
 
       val filesBefore = dataFilesIn(tableName).toSet
       val sql =
-        s"CALL system.rewrite_data_files(table => \"$tableName\", options => map('min-input-files','2'))"
+        s"""CALL system.rewrite_data_files(table => \"$tableName\",
+           |options => map('min-input-files','$MIN_INPUT_FILES'))""".stripMargin
       When(s"we execute the SQL ${formatSQL(sql)}")
       spark.sqlContext.sql(sql)
       val filesAfter = dataFilesIn(tableName).toSet
@@ -41,6 +46,19 @@ class OptimizationSpec extends SpecPretifier with GivenWhenThen with TableNameFi
       private val fromNewFiles: Array[Datum] = addedDf.collect()
       assert(fromNewFiles.length == num_rows)
       assert(diffHavingOrdered(fromNewFiles).isEmpty)
+    }
+    s"have snapshots removed when expired" in new SimpleSparkFixture {
+      val filesBefore = dataFilesIn(tableName).toSet
+      Given(s"there are already ${filesBefore.size} files for table $tableName")
+      val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
+      val sql =
+        s"""CALL iceberg.system.expire_snapshots(table => $tableName,
+           |older_than => TIMESTAMP '${dateFormat.format(new java.util.Date())}',
+           |retain_last => 5,
+           |stream_results => true);""".stripMargin
+      When(s"we execute the SQL:${formatSQL(sql)}")
+      val filesAfter = dataFilesIn(tableName).toSet
+      Then(s"old files have been removed and only ${filesAfter.size} remain")
     }
   }
 
