7e500376a5e68df79d751d3d9c02dbb09320fce8
PhillipHenry <PhillHenry@gmail.com>
2024-11-07 17:25:28 +0000

    Solves non determinism. Can now run: mvn post-integration-test  -Dsuites='uk.co.odinconsultants.iceberg.ConcurrentWriteSpec'

--- src/test/scala/uk/co/odinconsultants/iceberg/ConcurrentWriteSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/ConcurrentWriteSpec.scala
@@ -1,6 +1,6 @@
 package uk.co.odinconsultants.iceberg
 import org.apache.iceberg.spark.actions.SparkActions
-import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.{DataFrame, Dataset}
 import org.scalatest.GivenWhenThen
 import uk.co.odinconsultants.SparkForTesting.spark
 import uk.co.odinconsultants.documentation_utils.{Datum, SpecPretifier}
@@ -21,24 +21,30 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
 
   "Concurrent writes" should {
     "cause one transaction to fail" in new SimpleSparkFixture {
-      def writeData(): Future[Unit] = Future {
+      def writeData(): Future[Unit]        = Future {
+        val df: Dataset[Datum] = spark.createDataFrame(data).as[Datum]
+        df.map(ConcurrentWriteSpec.delayingFn(num_rows / 10)).writeTo(tableName).create()
+      }
+      def writeDataNoDelay(): Future[Unit] = Future {
         spark.createDataFrame(data).writeTo(tableName).create()
       }
       Given(s"two transactions trying to write data\n${prettyPrintSampleOf(data)}")
       When("both run at the same time")
-      val first                     = writeData()
-      val second                    = writeData()
-      val results                   = for {
+      val first                            = writeData()
+      val second                           = writeDataNoDelay()
+      val results                          = for {
         future <- List(first, second)
       } yield Await.ready(future, Duration(1, MINUTES))
-      val failures: List[Try[Unit]]  = results.flatMap(_.value.filter(_.isFailure).toList)
+      val failures: List[Try[Unit]]        = results.flatMap(_.value.filter(_.isFailure).toList)
       assert(failures.size == 1)
-      val failure = failures.map(_ match {
-        case Failure(exception) =>
-          exception.printStackTrace()
-          exception
-        case _                  => throw new Exception("Was expecting one TX to fail")
-      }).head
+      val failure                          = failures
+        .map(_ match {
+          case Failure(exception) =>
+            exception.printStackTrace()
+            exception
+          case _                  => throw new Exception("Was expecting one TX to fail")
+        })
+        .head
       Then(s"one fails with exception ${failure}")
       assert(failures.size == 1)
       And("one succeeds")
@@ -51,11 +57,11 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
       Given(s"the table '$tableName' has had a failed write")
       When(s"we call spark.read.table(\"$tableName\")")
       val table: Dataset[Datum] = spark.read.table(tableName).as[Datum]
-      private val nRows: Long = table.count()
+      private val nRows: Long   = table.count()
       Then(s"the table still contains $nRows records")
       assert(nRows == data.length)
       And("failed files are left behind")
-      val raw: Dataset[Datum] = spark.read.parquet(dataDir(tableName)).as[Datum]
+      val raw: Dataset[Datum]   = spark.read.parquet(dataDir(tableName)).as[Datum]
       assert(raw.count() > nRows)
     }
   }
@@ -63,7 +69,7 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
   "Orphans" should {
     "be deleted" in new SimpleSparkFixture {
       val filesBefore = Set(dataFilesIn(tableName))
-      val sql =
+      val sql         =
         s"""CALL system.remove_orphan_files(
            |    table => '$tableName')""".stripMargin
       When(s"we execute the SQL:${formatSQL(sql)}")
@@ -73,14 +79,13 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
 
       val dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")
 
-      /**
-       * java.lang.IllegalArgumentException: Cannot remove orphan files with an interval less
-       * than 24 hours. Executing this procedure with a short interval may corrupt the table if
-       * other operations are happening at the same time. If you are absolutely confident that no
-       * concurrent operations will be affected by removing orphan files with such a short interval,
-       * you can use the Action API to remove orphan files with an arbitrary interval.
-       *       at org.apache.iceberg.spark.procedures.RemoveOrphanFilesProcedure.validateInterval(RemoveOrphanFilesProcedure.java:209)
-       */
+      /** java.lang.IllegalArgumentException: Cannot remove orphan files with an interval less
+        * than 24 hours. Executing this procedure with a short interval may corrupt the table if
+        * other operations are happening at the same time. If you are absolutely confident that no
+        * concurrent operations will be affected by removing orphan files with such a short interval,
+        * you can use the Action API to remove orphan files with an arbitrary interval.
+        *       at org.apache.iceberg.spark.procedures.RemoveOrphanFilesProcedure.validateInterval(RemoveOrphanFilesProcedure.java:209)
+        */
 //      spark.sqlContext.sql(s"""CALL system.remove_orphan_files(
 //                              |    table => '$tableName',
 //                              |    older_than => TIMESTAMP '${dateFormat.format(new java.util.Date())}'
@@ -94,10 +99,8 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
 //      spark.sqlContext.sql(s"""CALL system.rewrite_data_files(
 //                              |    table => '$tableName')""".stripMargin)
 
-
-
       val actions = SparkActions.get(spark)
-      val action = actions.deleteOrphanFiles(icebergTable(tableName))
+      val action  = actions.deleteOrphanFiles(icebergTable(tableName))
       action.olderThan(new Date().getTime)
       action.execute()
 
@@ -108,4 +111,14 @@ class ConcurrentWriteSpec extends SpecPretifier with GivenWhenThen with TableNam
     }
   }
 
+}
+
+object ConcurrentWriteSpec {
+  def delayingFn(trigger: Int): Datum => Datum = { x =>
+    if (x.id == trigger) {
+      println("Pausing...")
+      Thread.sleep(2000)
+    }
+    x
+  }
 }
