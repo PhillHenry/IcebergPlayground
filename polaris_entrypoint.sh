f9b2b57d84b3f12df0d8a302ec13b0d5687cc7b8
PhillipHenry <PhillHenry@gmail.com>
2024-12-19 16:00:39 +0000

    Now works when a Docker user exists or not.

--- polaris_entrypoint.sh
+++ polaris_entrypoint.sh
@@ -1,5 +1,8 @@
-if [ "$RUN_USER_ID" != "" ] &>/dev/null; then
-  echo "Creating user with ID $RUN_USER_ID"
-  useradd -u $RUN_USER_ID -g 1000 polaris
+if [  $(grep $RUN_USER_ID /etc/passwd) ] ; then
+  su -c "/app/bin/polaris-dropwizard-service server polaris-server.yml"  $(id -un $RUN_USER_ID)
+elif [ "$RUN_USER_ID" != "" ] ; then
+  echo "Creating user with ID $RUN_USER_ID and GID $RUN_GROUP_ID"
+  groupadd -g $RUN_GROUP_ID tempgroup || true
+  useradd -u $RUN_USER_ID -g $RUN_GROUP_ID polaris
+  su -c "/app/bin/polaris-dropwizard-service server polaris-server.yml" polaris
 fi
-su -c "/app/bin/polaris-dropwizard-service server polaris-server.yml" polaris
