9ccc81d5f6d5f6c14217689618642ba3f8b50c6e
PhillipHenry <PhillHenry@gmail.com>
2025-01-28 11:09:32 +0000

    Refactored.

--- src/test/scala/uk/co/odinconsultants/iceberg/MergeIntoSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/MergeIntoSpec.scala
@@ -1,48 +1,56 @@
 package uk.co.odinconsultants.iceberg
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import org.scalatest.GivenWhenThen
-import uk.co.odinconsultants.documentation_utils.SpecPretifier
+import uk.co.odinconsultants.documentation_utils.{Datum, SpecPretifier}
 import uk.co.odinconsultants.iceberg.SQL.insertSQL
-import uk.co.odinconsultants.iceberg.TestUtils.{idField, labelField}
+import uk.co.odinconsultants.iceberg.TestUtils.{idField, labelField, partitionField}
 
 class OtherTable extends TableNameFixture
 
 class MergeIntoSpec extends SpecPretifier with GivenWhenThen with TableNameFixture with UpdatingTable {
 
+  info("https://github.com/apache/iceberg/issues/7431")
+
   def mode      = "merge-on-read"
+
+  def mergeSQL(targetTable: String, sourceTable: String): String =
+    s"""MERGE INTO $targetTable t USING (SELECT * FROM $sourceTable) s
+       |ON s.${idField} = t.${idField}
+       |WHEN MATCHED THEN UPDATE SET t.$labelField = s.$labelField
+       |WHEN NOT MATCHED THEN INSERT *""".stripMargin
+
+  def createTableAndPopulate(tableName: String,
+                             data: Seq[Datum],
+                             spark: SparkSession) = {
+    val createSQL: String = tableDDL(tableName, mode, partitionField)
+    val sql: String       = insertSQL(tableName, data)
+    spark.sqlContext.sql(createSQL)
+    spark.sqlContext.sql(sql)
+  }
 
   s"A $mode table" should {
     s"create no new files for $mode" in new SimpleSparkFixture {
+      import spark.implicits._
 
-      private def createTableAndPopulate(tableName: String) = {
-        val createSQL: String = tableDDL(tableName, mode, partitionField)
-        val sql: String       = insertSQL(tableName, data)
-        spark.sqlContext.sql(createSQL)
-        spark.sqlContext.sql(sql)
-      }
+      val otherTable = (new OtherTable).tableName
+      Given(s"tables $tableName and ${otherTable} both created as '${mode}' containing data that is the same other than the labels column")
+      createTableAndPopulate(tableName, data, spark)
+      val otherData = data.map(x => x.copy(label = s"${x.label}X"))
+      createTableAndPopulate(otherTable, otherData, spark)
 
-      val otherTable = new OtherTable
-      Given(s"tables $tableName and ${otherTable.tableName} both created as '${mode}'")
-      createTableAndPopulate(tableName)
-      createTableAndPopulate(otherTable.tableName)
-      s"""merge into $tableName t
-        |using( select *, row_number() over(partition by $partitionField order by modified desc,dt desc)
-        |as rank from spark_catalog.dw_source.s_std_trade_tidb where dt>='2023-04-20' )
-        |small where rank=1) s
-        |ON t.uni_order_id = s.uni_order_id and t.tenant = s.tenant and t.partner = s.partner
-        |WHEN MATCHED AND s.modified>=t.modified then
-        |UPDATE SET ....  WHEN NOT MATCHED THEN INSERT * ;""".stripMargin
-
-      val mergeSql =
-        s"""MERGE INTO $tableName t USING (SELECT * FROM ${otherTable.tableName}) s
-           |ON s.${idField} = t.${idField}
-           |WHEN MATCHED THEN UPDATE SET t.${labelField} = 'changed'
-           |WHEN NOT MATCHED THEN INSERT *""".stripMargin
+      val mergeSql = mergeSQL(tableName, otherTable)
       When(s"we merge them with:\n${formatSQL(mergeSql)}")
       val merged = spark.sqlContext.sql(mergeSql)
+      private val df: Dataset[Datum] =
+        spark.sqlContext.sql(s"SELECT * FROM $tableName ORDER BY $idField").as[Datum]
       Then(
         "the initial table looks like:\n" +
-        captureOutputOf(spark.sqlContext.sql(s"SELECT * FROM $tableName ORDER BY $idField").show(truncate = false))
+        captureOutputOf(df.show(truncate = false))
       )
+      Then(s"the target table $tableName has the labels from the source table $otherTable")
+      val dataPostMerge = df.collect()
+      assert(dataPostMerge.length == data.length)
+      assert(dataPostMerge.map(_.label).toSet == otherData.map(_.label).toSet)
     }
   }
 }
