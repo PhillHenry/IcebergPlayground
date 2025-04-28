792bdd3375c56277b1d6b4cb76c14aee429c8174
PhillHenry <PhillHenry@gmail.com>
2025-04-28 11:01:40 +0100

    Note, small write.spark.advisory-partition-size-bytes does change the number of threads!

--- src/test/scala/uk/co/odinconsultants/iceberg/HashDistributionSortedTableSpec.scala
+++ src/test/scala/uk/co/odinconsultants/iceberg/HashDistributionSortedTableSpec.scala
@@ -1,8 +1,11 @@
 package uk.co.odinconsultants.iceberg
 
 class HashDistributionSortedTableSpec extends HashDistributionSpec {
+  def fileByteSize: Long = 1024 * 1024 * 1024
   override protected def otherProperties(partitionField: String): Seq[String] =
     Seq(s"'sort-order' = '$partitionField ASC NULLS FIRST'",
-      s"'spark.sql.adaptive.advisoryPartitionSizeInBytes' = '${1024*1024*1024}'"
+      s"'spark.sql.adaptive.advisoryPartitionSizeInBytes' = '$fileByteSize'",
+      s"'write.spark.advisory-partition-size-bytes' = '$fileByteSize'",
+      s"'write.target-file-size-bytes' = '$fileByteSize'"
     )
 }
